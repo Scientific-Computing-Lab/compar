9b35d33fc561baab06b7753a56a922cf2a2c02ca
May Hagbi <mayhagbi@gmail.com>
2019-12-28 15:22:20 +0200

    add assert to exception that check if file from format txt

--- exceptions.py
+++ exceptions.py
@@ -8,3 +8,8 @@ class FileError(Exception):
 def assert_file_exist(file_path):
     if not os.path.exists(file_path):
         raise FileError('File {} not exist'.format(file_path))
+
+
+def assert_file_end_with_txt(file_path):
+    if os.path.basename(file_path).split('.')[1] != 'txt':
+        raise FileError('File {} should be in txt format'.format(file_path))
