439d4e6d0a734101e8f28037a2e83cd35128025f
Yoni Cohen <yonicohen1993@gmail.com>
2019-12-28 13:56:18 +0200

    add file exist assertion

--- exceptions.py
+++ exceptions.py
@@ -1,2 +1,10 @@
+import os
+
+
 class FileError(Exception):
     pass
+
+
+def assert_file_exist(file_path):
+    if not os.path.exists(file_path):
+        raise FileError('File {} not exist'.format(file_path))
