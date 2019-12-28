d8c1b68e1c13bd1ec77b3cd529c1e94a923f3622
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-12-28 17:39:58 +0200

    Integrate Autopar script in Autopar Class

--- compilers/autopar.py
+++ compilers/autopar.py
@@ -1,9 +1,11 @@
+import argparse
+import os
 import subprocess
+import distutils
+import time
 from compiler import Compiler
 
 class Autopar(Compiler):
-
-    autopar_script_path = "scripts/autoPar_script.py"
 
     def __init__(self,version,input_filename,output_filename,compilation_flags):
         Compiler.__init__(self,version,input_filename,output_filename,compilation_flags)
@@ -12,10 +14,23 @@ class Autopar(Compiler):
 
         # Parallelizing
         try:
-            process = subprocess.Popen(['bash','-c', "python3" + Autopar.autopar_script_path + "-dir " + self.get_input_filename() + " -options " + " ".join(str(x) for x in self.get_compilation_flags()) ])
-            process.wait()
+            dir = os.path.abspath(self.get_input_filename())
+            # dir_name = os.path.basename(dir)
+            for root, dirs, files in os.walk(dir):
+                for name in files:
+                    if os.path.splitext(name)[1] == '.c':
+                        print(name, dir)
+                        self.run_autopar(name, dir, self.get_compilation_flags())
             return True
-        except:
+        except Exception as e:
             print("files in directory " + self.get_input_filename() + " failed to be parallel!")
+            print(e)
             return False
 
+    def run_autopar(self, f_name, dest_dir, options):
+        print ("Parallelizing "+ f_name)
+        #sub_proc = subprocess.Popen(['autoPar'] + options + [f_name, f_name], cwd=dest_dir)
+        #sub_proc.wait()
+        print ("Done parallel work")
+
+x= Autopar(3,"dass","sdfdsf",["www"]).compile()
