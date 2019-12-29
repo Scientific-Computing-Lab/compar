eb355da7a08c78b1f9264c9a58a0b9f76a1e5f43
shlomi <shlomitofahi@gmail.com>
2019-12-29 17:38:32 +0200

    fix syntax

--- executor.py
+++ executor.py
@@ -1,26 +1,26 @@
-class Job :
-    def __init__(self,file_path, binary_file_path, combination =None, sections_runtime =None):
+class Job:
+    def __init__(self,file_path, binary_file_path, combination=None, sections_runtime=None):
         self.file_path = file_path
-        self.binary_file_path=binary_file_path
-        self.combination=combination
-        #(compiler_type,compile_flags,env_var)
-        self.sections_runtime=sections_runtime
+        self.binary_file_pat = binary_file_path
+        self.combination = combination
+        self.sections_runtime = sections_runtime
+         #(compiler_type,compile_flags,env_var)
 
 
-class Executor :
-    def __init__(self,serial_job,jobs):
-        self.serial_job=serial_job
-        self.jobs=jobs
-
+class Executor:
+    def __init__(self, serial_job, jobs):
+        self.serial_job = serial_job
+        self.jobs = jobs
 
     def execute_job(self):
 
         #run serial
         for job in self.jobs:
-            #srat sbach and white using thread
-            #
+            pass
+            #start sbach and white using thread
 
-
-    def __analiysis_output_file(self):
+    def __analysis_output_file(self):
+        pass
 
     def __send_to_database(self):
+        pass
