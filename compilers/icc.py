122cdbb24284eeed255dcc2a38ee91be6f79b654
Gilad Guralnik <giladguralnik1994@gmail.com>
2019-12-29 23:30:42 +0200

    repair class to commit

--- compilers/icc.py
+++ compilers/icc.py
@@ -9,25 +9,4 @@ class icc(Compiler):
 
     def compile(self):
 
-        # Parallelizing
-        try:
-            dir = os.path.abspath(self.get_input_file_directory())
-            # dir_name = os.path.basename(dir)
-            for root, dirs, files in os.walk(dir):
-                for name in files:
-                    if os.path.splitext(name)[1] == '.c':
-                        Autopar.run_autopar(name, dir, self.get_compilation_flags())
-            return True
-
-        except Exception as e:
-            print("files in directory " + self.get_input_file_directory() + " failed to be parallel!")
-            print(e)
-            return False
-
-
-    @staticmethod
-    def run_icc(self, files_dir, dest_dir, options):
-        print("Compiling " + files_dir)
-        sub_proc = subprocess.Popen(['icc'] + options + [files_dir, files_dir], cwd=dest_dir)
-        sub_proc.wait()
-        print("Done Compile work")
+        pass
