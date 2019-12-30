05de374d7a39f25680230e6e57eec62d5813f6e5
yoel_vaizman <yoelv92@gmail.com>
2019-12-30 17:58:13 +0200

    creating the pool thread and opening class processor and change job class

--- executor.py
+++ executor.py
@@ -1,52 +1,97 @@
 import os, subprocess
 import run_job
+import threading
+from concurrent.futures import ThreadPoolExecutor
 
 class Job:
-    def __init__(self, file_path, binary_file_path, args=None, combination=None, sections_runtime=None):
-        self.file_path = file_path
-        self.binary_file_path = binary_file_path
+    '''
+    dir - is the name of the directory that contion the files of the job
+    '''
+    def __init__(self, directory, args=None, combination=None, sections_runtime={}):
+        self.directory = directory
         self.combination = combination
         self.sections_runtime = sections_runtime
         self.args = args
         #(compiler_type,compile_flags,env_var)
 
-    def get_file_path(self):
-        return self.file_path
-
-    def get_binary_file_path(self):
-        return self.binary_file_path
+    def get_dir(self):
+        return self.directory
 
     def get_args(self):
         return self.args
 
+    def get_sections_runtime(self):
+        return self.sections_runtime
+
+    def get_loop_time(self,loop_name):
+        return self.sections_runtime[loop_name]
+
+    def set_sections_runtime(self, loop_name, time):
+        self.sections_runtime[loop_name]=time
+
+class Processor:
+
+    def __init__(self, job):
+        self.job = job
+
+    def get_job(self):
+        return self.job
+
+    def run(self):
+        self.__run_with_sbatch()
+        self.analysis_output_file()
+
+    def __run_with_sbatch(self, times=1, slurm_partition='grid'):
+        '''
+        :param times: The number of times you want to run the Job. Default=1
+        :param slurm_partition:The SLURM option . default="grid"
+        :return:
+        Look for an .x file inside the folder of Job (job.get_dir()).
+        Send it to run on sbatch.
+        Save the output files in the Job dir.
+        '''
+        batch_file = self.__make_batch_file()
+        for root, dirs, files in os.walk(self.get_job().get_dir()):
+            for file in files:
+                if os.path.splitext(file)[1] == '.x':
+                    file_name = os.path.basename(os.path.splitext(file)[0])
+                    file_path = os.path.abspath(file)
+                    for i in range(0, times):
+                        output_file_name = file_name + '_' + str(i)
+                        sub_proc = subprocess.Popen(
+                            ['sbatch', '-p', slurm_partition, '-o', output_file_name, '-J', output_file_name,
+                             batch_file, file_path, self.get_job().get_args()], cwd=self.get_job().get_dir())
+
+                        sub_proc.wait()
+
+    def __make_batch_file(self):
+        batch_file_path = os.path.join(os.path.abspath(self.get_job().get_dir()), 'batch_job.sh')
+        batch_file = open(batch_file_path, 'w')
+        batch_file.write(
+            '#!/bin/bash\n'
+            + '#SBATCH --exclusive\n'
+            + '$@\n'
+        )
+
+    def __analysis_output_file(self):
+        for root, dirs, files in os.walk(self.get_job().get_dir()):
+            for file in files:
+                if os.path.splitext(file)[1] == '.txt':
+                    f = open(file, "r")
+                    for x in f:
+                        if(x == "what yoni or may choose (LOOP name #time) "):
+                            #merfassim !!!
+
 
 
 class Executor:
-
     @staticmethod
-    def execute_job(serial_job, jobs):
+    def execute_jobs(jobs, number_of_threads=4):
+        pool = ThreadPoolExecutor(max_workers=number_of_threads)
+        for job in jobs():
+            pool.submit(Processor(job).run())
+        pool.shutdown()
+        return jobs
 
-       output_file=run_job.run_with_sbatch(serial_job.get_binary_file_path(),serial_job.get_args())
-       output_file_path = os.path.abspath(output_file)
 
-
-        for job in jobs:
-
-        #run serial
-        batch_file = self.__make_batch_file(self.serial_job.get_binary_file_path())
-        n = 0
-
-        for job in self.serial_job+self.jobs:
-            job_name = os.path.splitext(job.get_file_path())[0]
-            job_des = job.get_binary_file_path()
-
-            sub_proc = subprocess.Popen(['sbatch', '-p', 'grid', '-o', job_name, '-J', job_name,
-                                         batch_file, job_des, '-n', str(n), '-j', job_name], cwd=job_des)
-            sub_proc.wait()
-
-    def __analysis_output_file(self):
-        pass
-
-    def __send_to_database(self):
-        pass
 
