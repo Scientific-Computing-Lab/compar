11e8fef312b62c7a1693a3d75c6e25648a1e2a11
Yoni Cohen <yonicohen1993@gmail.com>
2020-01-04 15:52:02 +0200

    add compilation error exception

--- exceptions.py
+++ exceptions.py
@@ -5,6 +5,10 @@ class FileError(Exception):
     pass
 
 
+class CompilationError(Exception):
+    pass
+
+
 def assert_file_exist(file_path):
     if not os.path.exists(file_path):
         raise FileError('File {} not exist'.format(file_path))
