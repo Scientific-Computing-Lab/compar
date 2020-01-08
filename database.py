afbf2f588531eb4c9229fdc4bc30567b67c316ad
Gilad Guralnik <giladguralnik1994@gmail.com>
2020-01-08 13:39:10 +0200

    minor changes -  DB class

--- database.py
+++ database.py
@@ -1,29 +1,40 @@
 import pymongo
 from bson import json_util
 
+STATIC_DB_NAME = "combinations"
+STATIC_COLLECTION_NAME = "combinations"
+DYNAMIC_DB_NAME = "results"
+DB = "mongodb://localhost:27017"
+
 
 class Database:
 
-    def __init__(self):
+    def __init__(self, collection_name, file_path):
         self.current_combination = None
         self.current_combination_id = 0
-        self.connection = pymongo.MongoClient('mongodb://localhost:27017')
-        self.static_db = self.connection['combinations']
-        self.dynamic_db = self.connection['results']
+        self.collection_name = collection_name
+        self.connection = pymongo.MongoClient(DB)
+        self.static_db = self.connection[STATIC_DB_NAME]
+        self.dynamic_db = self.connection[DYNAMIC_DB_NAME]
 
-        if 'combinations' not in self.static_db.list_collection_names():
-            self.static_db.create_collection('combinations')
-            self.initialize_static_db("file_name", "file_directory")
+        if STATIC_COLLECTION_NAME not in self.static_db.list_collection_names():
+            self.static_db.create_collection(STATIC_COLLECTION_NAME)
+            self.initialize_static_db(file_path)
 
-        if 'results' not in self.dynamic_db.list_collection_names():
-            self.dynamic_db.create_collection('results')
+        if self.collection_name not in self.dynamic_db.list_collection_names():
+            self.dynamic_db.create_collection(self.collection_name)
+        else:
+            raise Exception("results DB already has {0} name collection!".format(self.collection_name))
 
     def initialize_static_db(self, file_path):
         try:
             f = open(file_path, "r")
             comb_array = json_util.loads(f.read())
+            current_id = 0
             for comb in comb_array:
-                self.static_db['combinations'].insert_one(comb)
+                comb["_id"] = current_id
+                self.static_db[STATIC_COLLECTION_NAME].insert_one(comb)
+                current_id += 1
             return True
 
         except Exception as e:
@@ -32,103 +43,49 @@ class Database:
             return False
 
     def get_next_combination(self):
-        self.current_combination = self.static_db["combinations"].find_one({"_id": self.current_combination_id})
+        self.current_combination = self.static_db[STATIC_COLLECTION_NAME].find_one({"_id": self.current_combination_id})
         self.current_combination_id += 1
         return self.current_combination
 
-    def insert_new_combination(self, collection_name, combination_result):
+    def insert_new_combination(self, combination_result):
         try:
-            self.dynamic_db[collection_name].insert_one(combination_result)
+            self.dynamic_db[self.collection_name].insert_one(combination_result)
             return True
         except Exception as e:
             print("Combination insertion failed")
             print(e)
             return False
 
-    def delete_combination(self, collection_name, combination_id):
+    def delete_combination(self, combination_id):
         try:
-            self.dynamic_db[collection_name].delete_one({"_id": combination_id})
+            self.dynamic_db[self.collection_name].delete_one({"_id": combination_id})
             return True
         except Exception as e:
             print("Could not delete combination")
             print(e)
             return False
 
-    def find_optimal_loop_combination(self, collection_name, file_name, loop_label):
+    def find_optimal_loop_combination(self, file_name, loop_label):
         best_speedup = 1
         best_combination_id = -1
-        combinations = self.dynamic_db[collection_name].find({})
+        combinations = self.dynamic_db[self.collection_name].find({})
         for combination in combinations:
             for file in combination['run_time_results']:
-                if file['file_name'] == file_name :
+                if file['file_name'] == file_name:
                     for loop in file['loops']:
-                        if loop['loop_label'] == loop_label :
+                        if loop['loop_label'] == loop_label:
                             if loop['speedup'] > best_speedup:
                                 best_speedup = loop['speedup']
-                                best_combination_id = combination['combination_id']
+                                best_combination_id = combination['_id']
 
         return best_combination_id
 
     def get_combination_from_static_db(self, combination_id):
         combination = None
         try:
-            combination = self.static_db["combinations"].find_one({"_id": combination_id})
+            combination = self.static_db[STATIC_COLLECTION_NAME].find_one({"_id": combination_id})
         except Exception as e:
             print("Could not find combination")
             print(e)
         finally:
             return combination
-
-
-# ~~~~~~~~~ Examples
-x = Database()
-# test = x.find_optimal_loop_combination('Autopar', 'bin.c', 'l1')
-# print(test)
-
-#x.initialize_static_db(r"C:\Users\Gilad\Desktop\data.json")
-
-print(x.get_next_combination())
-print(x.get_next_combination())
-# x.create_new_collection('Autopar')
-# x.insert_new_combination('Autopar',{'test': 'test'})
-
-#file example:
-'''
-[
-{
-	"compiler": "cetus",
-	"env_params": [
-		"sd",
-		"bb",
-		"rr"
-	],
-	"code_params": [
-		"nn",
-		"jj",
-		"oo"
-	],
-	"compiler_params": [
-		"pp",
-		"uu"
-	],
-	"_id": 2
-},
-{
-	"compiler": "autopar",
-	"env_params": [
-		"sd",
-		"bb",
-		"rr"
-	],
-	"code_params": [
-		"nn",
-		"jj",
-		"oo"
-	],
-	"compiler_params": [
-		"pp",
-		"uu"
-	],
-	"_id": 1
-}
-]'''
