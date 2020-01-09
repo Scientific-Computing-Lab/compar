0ffabd2bef3ae02ca3d091c4f78ede77661ab4c1
shlomi <shlomitofahi@gmail.com>
2020-01-09 17:35:44 +0200

    run_serial change: change arg call from '1' (string) to 1.0 (float) in set_loop_speedup_in_file_results

--- compar.py
+++ compar.py
@@ -495,7 +495,7 @@ class Compar:
         for file in self.make_absolute_file_list(serial_dir_path):
             run_time_result_loops = job.get_file_results_loops(file['file_name'])
             for loop in run_time_result_loops:
-                job.set_loop_speedup_in_file_results(file_name=file, loop_label=loop['loop_label'], speedup='1')
+                job.set_loop_speedup_in_file_results(file_name=file, loop_label=loop['loop_label'], speedup=1.0)
 
                 key = file['file_name'], loop['loop_label']
                 value = loop['run_time']
