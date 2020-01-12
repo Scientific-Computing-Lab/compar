e67fdedd61fb497e897d1549477ef2678caac1a5
Yoel Vaizman <yoelv92@gmail.com>
2020-01-12 20:44:53 +0200

    fixing cetus compiler by remove #include<opm.h> from the C code and inject #include<omp.h> after compiling

--- compilers/cetus.py
+++ compilers/cetus.py
@@ -3,6 +3,7 @@ from exceptions import CompilationError
 import subprocess
 import shutil
 import os
+import exceptions as e
 
 
 class Cetus(ParallelCompiler):
@@ -14,6 +15,7 @@ class Cetus(ParallelCompiler):
         super().compile()
         try:
             for file in self.get_file_list():
+                Cetus.replace_line_in_code(file["file_full_path"], '#include <omp.h>', '')
                 cwd_path = os.path.dirname(file["file_full_path"])
                 subprocess.run(['module load cetus; cetus {} {}'.format(" ".join(self.get_compilation_flags()), file["file_name"])],
                                shell=True, cwd=cwd_path)
@@ -23,7 +25,33 @@ class Cetus(ParallelCompiler):
                     dst_file = file["file_full_path"]
                     shutil.copy(src_file, dst_file)
                     shutil.rmtree(os.path.join(cwd_path, "cetus_output"))
+
+                Cetus.inject_line_in_code(file["file_full_path"], '#include <omp.h>')
             return True
 
         except Exception as e:
             raise CompilationError(str(e) + " files in directory " + self.get_input_file_directory() + " failed to be parallel!")
+
+    @staticmethod
+    def replace_line_in_code(file_full_path, old_line, new_line):
+        with open(file_full_path, 'r') as input_file:
+            c_code = input_file.read()
+        e.assert_file_is_empty(c_code)
+        c_code = c_code.replace(old_line, new_line)
+        try:
+            with open(file_full_path, 'w') as output_file:
+                output_file.write(c_code)
+        except OSError as err:
+            raise e.FileError(str(err))
+
+    @staticmethod
+    def inject_line_in_code(file_full_path, new_line):
+        with open(file_full_path, 'r') as input_file:
+            c_code = input_file.read()
+        e.assert_file_is_empty(c_code)
+        c_code = new_line + c_code
+        try:
+            with open(file_full_path, 'w') as output_file:
+                output_file.write(c_code)
+        except OSError as err:
+            raise e.FileError(str(err))
