22a059c4aed763af6e53d89c470df60395718adf
Yoni Cohen <yonicohen1993@gmail.com>
2020-03-04 22:09:56 +0200

    Icc - add module load intel command - not tested!

--- compilers/icc.py
+++ compilers/icc.py
@@ -1,4 +1,6 @@
 from compilers.binaryCompiler import BinaryCompiler
+import os
+import subprocess
 
 
 class Icc(BinaryCompiler):
@@ -6,3 +8,14 @@ class Icc(BinaryCompiler):
     def __init__(self, version, compilation_flags=None, main_c_file=None, input_file_directory=None):
         super().__init__("icc", version, compilation_flags, main_c_file, input_file_directory)
 
+    def run_compiler(self):
+        input_file_path_only = os.path.dirname(self.get_input_file_directory() + os.path.sep)
+        dir_name = os.path.basename(input_file_path_only)
+
+        print("Compiling " + self.get_main_c_file())
+        output = subprocess.check_output(['module load intel', '&&', self.get_compiler_name()] + ["-fopenmp"]
+                                         + self.get_compilation_flags() + [self.get_main_c_file()]
+                                         + ["-o"] + [dir_name + ".x"],
+                                         cwd=self.get_input_file_directory())
+        print(self._compiler_name + ' compilation output: ' + str(output))
+        print("Done Compile work")
