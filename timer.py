600a7f0994628c27fdcacf29ae9dfcdb0f36288d
Reuven Regev Farag <regevfarag@gmail.com>
2020-03-07 14:28:09 +0200

    Change I/O logic  - Class Timer - part1

--- timer.py
+++ timer.py
@@ -1,6 +1,7 @@
 import os
 from fragmentator import Fragmentator
 import exceptions as e
+import re
 
 
 class Timer(object):
@@ -11,13 +12,16 @@ class Timer(object):
     DECL_START_TIME_VAR_CODE = 'double ' + COMPAR_VAR_PREFIX + 'start_time_{};\n'
     DECL_RUN_TIME_VAR_CODE = 'double ' + COMPAR_VAR_PREFIX + 'run_time_{};\n'
     DECL_FILE_POINTER_VAR_CODE = 'FILE *' + COMPAR_VAR_PREFIX + 'fp{};\n'
+    DECL_GLOBAL_STRUCT_CODE = 'typedef struct ____compar____struct {' \
+                              '\n\tint counter;\n\tfloat total_runtime;\n} ____compar____struct;\n'
     INIT_START_TIME_VAR_CODE = COMPAR_VAR_PREFIX + 'start_time_{} = omp_get_wtime();\n'
     INIT_RUN_TIME_VAR_CODE = COMPAR_VAR_PREFIX + 'run_time_{} = omp_get_wtime() - ' +\
                              COMPAR_VAR_PREFIX + 'start_time_{};\n'
-    WRITE_TO_FILE_CODE = COMPAR_VAR_PREFIX + 'fp{} = fopen(\"' + __PREFIX_OUTPUT_FILE + '{}\", \"a\");\n'
-    WRITE_TO_FILE_CODE += 'fprintf(' + COMPAR_VAR_PREFIX + 'fp{}, \"run time of loop %d: %lf\\n\", {}, ' + \
-                          COMPAR_VAR_PREFIX + 'run_time_{});\n'
-    WRITE_TO_FILE_CODE += 'fclose(' + COMPAR_VAR_PREFIX + 'fp{});\n'
+
+    # WRITE_TO_FILE_CODE = COMPAR_VAR_PREFIX + 'fp{} = fopen(\"' + __PREFIX_OUTPUT_FILE + '{}\", \"a\");\n'
+    # WRITE_TO_FILE_CODE += 'fprintf(' + COMPAR_VAR_PREFIX + 'fp{}, \"run time of loop %d: %lf\\n\", {}, ' + \
+    #                       COMPAR_VAR_PREFIX + 'run_time_{});\n'
+    # WRITE_TO_FILE_CODE += 'fclose(' + COMPAR_VAR_PREFIX + 'fp{});\n'
 
     @staticmethod
     def get_file_name_prefix_token():
@@ -28,7 +32,7 @@ class Timer(object):
         declaration_code = '\n'
         declaration_code += Timer.DECL_START_TIME_VAR_CODE.format(label)
         declaration_code += Timer.DECL_RUN_TIME_VAR_CODE.format(label)
-        declaration_code += Timer.DECL_FILE_POINTER_VAR_CODE.format(label)
+        #TODO: declaration_code += Timer.DECL_FILE_POINTER_VAR_CODE.format(label)
         return declaration_code
 
     @staticmethod
@@ -42,8 +46,17 @@ class Timer(object):
     def get_suffix_loop_code(label, file_name):
         suffix_loop_code = '\n'
         suffix_loop_code += Timer.INIT_RUN_TIME_VAR_CODE.format(label, label)
-        suffix_loop_code += Timer.WRITE_TO_FILE_CODE.format(label, file_name, label, label, label, label)
+        # TODO: inject here to the new arrays
+        # TODO: suffix_loop_code += Timer.WRITE_TO_FILE_CODE.format(label, file_name, label, label, label, label)
         return suffix_loop_code
+
+    @staticmethod
+    def inject_global_declaration(input_file_text, num_of_loops, array_var_index):
+        new_code = Timer.DECL_GLOBAL_STRUCT_CODE
+        name_of_global_array = f'____compar____arr{str(array_var_index)}'
+        new_code += f"____compar____struct extern {name_of_global_array}[{num_of_loops}];\n"
+        new_code += input_file_text
+        return new_code
 
     def __init__(self, file_path):
         e.assert_file_exist(file_path)
@@ -79,12 +92,18 @@ class Timer(object):
     def set_number_of_loops(self, num_of_loops):
         self.__number_of_loops = num_of_loops
 
-    def inject_timers(self):
+    def calculate_num_of_loops(self):
         fragments = self.__fragmentation.fragment_code()
         with open(self.__input_file_path, 'r') as input_file:
             input_file_text = input_file.read()
         e.assert_file_is_empty(input_file_text)
         self.set_number_of_loops(len(fragments))
+        return input_file_text, fragments
+
+    def inject_timers(self, array_var_index, main_file_path):
+        input_file_text, fragments = self.calculate_num_of_loops()
+        if self.__input_file_path != main_file_path:
+            input_file_text = Timer.inject_global_declaration(input_file_text, self.__number_of_loops, array_var_index)
         if '#include <omp.h>' not in input_file_text:
             input_file_text = '#ifdef _OPENMP\n#include <omp.h>\n#endif\n{}'.format(input_file_text)
         if '#include <stdio.h>' not in input_file_text:
@@ -109,3 +128,41 @@ class Timer(object):
                 output_file.write(input_file_text)
         except OSError as err:
             raise e.FileError(str(err))
+
+    @staticmethod
+    def inject_code_to_main_file(main_file_path, declaration_code_to_inject):
+        with open(main_file_path, 'r') as input_file:
+            input_file_text = Timer.DECL_GLOBAL_STRUCT_CODE
+            input_file_text += declaration_code_to_inject + "\n"
+            input_file_text += input_file.read()
+
+            regex_pattern = "((int|void)[ ]*main[ ]*[(].*[)][ ]*[\\n]?{\\n)"
+            code_to_replace = re.findall(regex_pattern, input_file_text)
+            if len(code_to_replace) < 1:
+                # TODO: throw exception
+                pass
+
+            code_to_replace = code_to_replace[0][0]
+
+            new_code = Timer.generate_at_exit_function_code()
+            new_code += f'{code_to_replace} atexit(____compar____atExit);\n'
+
+            c_code = re.sub(regex_pattern, new_code, input_file_text)
+            with open(main_file_path, 'w') as output_file:
+                output_file.write(c_code)
+
+    @staticmethod
+    def generate_at_exit_function_code():
+        # TODO: onExit: open and write the results to files
+        # TODO: update the absolute paths after combination folder exist
+        code = 'void ____compar____atExit() {\n'
+
+        # Open files
+
+
+
+        # Write time to files
+        # Close files
+
+        code += '}\n'
+        return code
