8148adb53e98bc535d0316bd39451dfbd7489216
May Hagbi <mayhagbi@gmail.com>
2020-03-12 23:02:21 +0200

    update gui and logic stuff

--- compar_app/app.py
+++ compar_app/app.py
@@ -4,26 +4,36 @@ from flask_wtf import Form
 from wtforms import StringField, SubmitField, FileField, RadioField, TextAreaField
 from wtforms.validators import DataRequired
 from wtforms.widgets import TextArea
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
+bootstrap = Bootstrap(app)
 
 DATA = {}
 
 
 class BasicComparForm(Form):
     work_dir = StringField('Write path for working directory: ', validators=[DataRequired()])
-    input_dir = StringField('Write path for input directory: ', validators=[DataRequired()])
+    slum_params = StringField('Slum parameters: ', validators=[DataRequired()])
+    save = RadioField('Save folders?', choices=[('yes', 'Yes'), ('no', 'No')], default='yes')
     make_file_choice = RadioField('Do you want make file?', choices=[('yes', 'Yes'), ('no', 'No')], default='no')
     submit1 = SubmitField('Next')
 
 
 class MakeFileForm(Form):
-    commands = StringField('Commands: ', validators=[DataRequired()])
-    location = StringField('Location: ', validators=[DataRequired()])
-    file_name = StringField('File name: ', validators=[DataRequired()])
+    input_dir = StringField('Write path for input directory: ', validators=[DataRequired()])
+    ignore = StringField('relative folder paths to be ignored: ', validators=[DataRequired()])
+    include = StringField('Include dir names for compilation (relative paths): ', validators=[DataRequired()])
+    make_commands = StringField('Makefile Commands: ', validators=[DataRequired()])
+    make_op = StringField('Makefile output executable folder (relative path): ', validators=[DataRequired()])
+    make_on = StringField('Makefile output executable file name: ', validators=[DataRequired()])
+    main_file = StringField('Main c file name: ', validators=[DataRequired()])
+    main_file_p = StringField('Main c file parameters: ', validators=[DataRequired()])
+    main_file_r_p = StringField('Main c file name relative path: ', validators=[DataRequired()])
+    nas_file = RadioField('NAS file?', choices=[('yes', 'Yes'), ('no', 'No')], default='no')
 
 
 class BinaryCompilerForm(Form):
@@ -39,24 +49,19 @@ class CompilersForm(Form):
 
 
 class ExistsCFileForm(Form):
-    c_file_name = StringField('File name: ', validators=[DataRequired()])
-    c_file_parameters = StringField('Parameters: ')
+    input_dir = StringField('Write path for input directory: ', validators=[DataRequired()])
+    main_file = StringField('Main c file name: ', validators=[DataRequired()])
+    main_file_p = StringField('Main c file parameters: ', validators=[DataRequired()])
     nas_file = RadioField('NAS file?', choices=[('yes', 'Yes'), ('no', 'No')], default='no')
-    c_file_path = FileField('Choose file path: ')
+    main_file_r_p = StringField('Main c file name relative path: ', validators=[DataRequired()])
 
 
 class EditorCForm(Form):
     editor = StringField(u'Write C code', widget=TextArea())
 
 
-class FlagsForm(Form):
-    save = RadioField('Save folders?', choices=[('yes', 'Yes'), ('no', 'No')], default='yes')
-    ignore = StringField('Ignore: ')
-    include = StringField('Include: ')
-
-
 class DoYouHaveExistsFileForm(Form):
-    exists_file = RadioField('Do you have an exists file?', choices=[('yes', 'Yes'), ('no', 'No')], default='yes')
+    exists_file = RadioField('Do you have an exists files?', choices=[('yes', 'Yes'), ('no', 'No')], default='yes')
     submit2 = SubmitField('Next')
 
 
@@ -75,7 +80,6 @@ def welcome_label():
     make_form = MakeFileForm()
     binary_comp_form = BinaryCompilerForm()
     comp_form = CompilersForm()
-    flags_form = FlagsForm()
     exists_file_form = DoYouHaveExistsFileForm()
     start_form = StartForm()
     exists_c_file_form = ExistsCFileForm()
@@ -83,19 +87,7 @@ def welcome_label():
     if request.method == 'POST':
         if main_form.submit1.data:
             DATA['work_dir'] = request.form.get('work_dir')
-            DATA['input_dir'] = request.form.get('input_dir')
-            makefile_choice = request.form.get('make_file_choice')
-            if is_equal_to_yes(makefile_choice):
-                DATA['make_file'] = True
-                return render_template('home.html', form=main_form, make_form=make_form, start_form=start_form)
-            else:
-                DATA['make_file'] = False
-                return render_template('home.html', form=main_form, binary_comp_form=binary_comp_form,
-                                       comp_form=comp_form, flags_form=flags_form, exists_file_form=exists_file_form)
-        elif exists_file_form.submit2.data:
-            DATA['binary_comp_name'] = request.form.get('binary_comp_name')
-            DATA['binary_comp_version'] = request.form.get('binary_comp_version')
-            DATA['binary_comp_flags'] = request.form.get('binary_comp_flags')
+            DATA['slum_params'] = request.form.get('slum_params')
             DATA['p4a_flags'] = request.form.get('p4a')
             DATA['autopar_flags'] = request.form.get('autopar')
             DATA['cetus_flags'] = request.form.get('cetus')
@@ -103,19 +95,30 @@ def welcome_label():
                 DATA['save_folder'] = True
             else:
                 DATA['save_folder'] = False
-            DATA['ignore'] = request.form.get('ignore')
-            DATA['include'] = request.form.get('include')
-            exists_file_choice = request.form.get('exists_file')
-            if is_equal_to_yes(exists_file_choice):
+            if is_equal_to_yes(request.form.get('make_file_choice')):
+                DATA['is_make_file'] = True
+                return render_template('index.html', main_form=main_form, comp_form=comp_form, make_form=make_form,
+                                       start_form=start_form)
+            else:
+                DATA['is_make_file'] = False
+                return render_template('index.html', main_form=main_form, comp_form=comp_form,
+                                       binary_comp_form=binary_comp_form, exists_file_form=exists_file_form)
+        elif exists_file_form.submit2.data:
+            DATA['binary_comp_name'] = request.form.get('binary_comp_name')
+            DATA['binary_comp_version'] = request.form.get('binary_comp_version')
+            DATA['binary_comp_flags'] = request.form.get('binary_comp_flags')
+            if is_equal_to_yes(request.form.get('exists_file')):
                 DATA['exists_c_file'] = True
-                return render_template('home.html', form=main_form, binary_comp_form=binary_comp_form,
-                                       comp_form=comp_form, flags_form=flags_form, exists_file_form=exists_file_form,
+                return render_template('index.html', main_form=main_form, comp_form=comp_form,
+                                       binary_comp_form=binary_comp_form, exists_file_form=exists_file_form,
                                        exists_c_file_form=exists_c_file_form, start_form=start_form)
             else:
-                return render_template('home.html', form=main_form, binary_comp_form=binary_comp_form,
-                                       comp_form=comp_form, flags_form=flags_form, exists_file_form=exists_file_form,
+                return render_template('index.html', main_form=main_form, comp_form=comp_form,
+                                       binary_comp_form=binary_comp_form, exists_file_form=exists_file_form,
                                        editor_c_form=editor_c_form, start_form=start_form)
-    return render_template('home.html', form=main_form)
+        elif start_form.submit3.data:
+            pass
+    return render_template('index.html', main_form=main_form, comp_form=comp_form)
 
 
 # @app.route("/make_choice", methods=["GET", "POST"])
