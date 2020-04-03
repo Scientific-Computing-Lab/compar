a4877ce1979db20d0e065d932e21222bcfd96f43
Reuven Regev Farag <regevfarag@gmail.com>
2020-04-03 15:12:51 +0300

    subprocess_handler- in case of argument list convert to string

--- subprocess_handler.py
+++ subprocess_handler.py
@@ -3,7 +3,9 @@ import os
 
 
 def run_subprocess(command, cwd=os.curdir):
-    pipes = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd)
+    if isinstance(command, list):
+        command = " ".join(command)
+    pipes = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, shell=True)
     std_out, std_err = pipes.communicate()
     std_out, std_err = str(std_out, encoding='utf-8'), str(std_err, encoding='utf-8')
     return std_out, std_err, pipes.returncode
