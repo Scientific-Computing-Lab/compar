9041962b6278d64d6c6b6d1aad89dc964b43239b
Reuven Regev Farag <regevfarag@gmail.com>
2020-04-04 22:11:30 +0300

    kill subprocess when they finished

--- subprocess_handler.py
+++ subprocess_handler.py
@@ -7,5 +7,8 @@ def run_subprocess(command, cwd=os.curdir):
         command = " ".join(command)
     pipes = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, shell=True)
     std_out, std_err = pipes.communicate()
+    return_code = pipes.returncode
+    pipes.kill()
+    del pipes
     std_out, std_err = str(std_out, encoding='utf-8'), str(std_err, encoding='utf-8')
-    return std_out, std_err, pipes.returncode
+    return std_out, std_err, return_code
