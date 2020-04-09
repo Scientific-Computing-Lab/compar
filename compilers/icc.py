1936e5e015d66b18f30ea84d7cd072e2c6571490
Yoni Cohen <yonicohen1993@gmail.com>
2020-04-09 15:41:18 +0300

    Icc - add verbose printing

--- compilers/icc.py
+++ compilers/icc.py
@@ -1,7 +1,7 @@
 from compilers.binaryCompiler import BinaryCompiler
 import os
-import subprocess
 from subprocess_handler import run_subprocess
+import logger
 
 
 class Icc(BinaryCompiler):
@@ -13,9 +13,10 @@ class Icc(BinaryCompiler):
         input_file_path_only = os.path.dirname(self.get_input_file_directory() + os.path.sep)
         dir_name = os.path.basename(input_file_path_only)
 
-        print("Compiling " + self.get_main_c_file())
+        logger.info(f'{Icc.__name__} start to compiling {self.get_main_c_file()}')
         stdout, stderr, ret_code = run_subprocess([self.get_compiler_name()] + ["-fopenmp"]
                                          + self.get_compilation_flags() + [self.get_main_c_file()]
                                          + ["-o"] + [dir_name + ".x"], self.get_input_file_directory())
-        print(self._compiler_name + ' compilation output: ' + str(stdout))
-        print("Done Compile work")
+        logger.debug(stdout)
+        logger.debug_error(stderr)
+        logger.info(f'{Icc.__name__} finish to compiling {self.get_main_c_file()}')
