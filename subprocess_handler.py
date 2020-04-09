304a008b21a5fd5684494cfcac9d6771046e07e5
Yoni Cohen <yonicohen1993@gmail.com>
2020-04-09 17:20:15 +0300

    subprocess_handler - printing the execution command

--- subprocess_handler.py
+++ subprocess_handler.py
@@ -1,10 +1,12 @@
 import subprocess
 import os
+import logger
 
 
 def run_subprocess(command, cwd=os.curdir):
     if isinstance(command, list):
         command = " ".join(command)
+    logger.info(f'Running {command} command')
     pipes = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, shell=True,
                              env=os.environ)
     std_out, std_err = pipes.communicate()
