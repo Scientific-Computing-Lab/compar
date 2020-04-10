9e323df30d32d77888c6ced23f222d4ab2a82b73
Gilad Guralnik <giladguralnik1994@gmail.com>
2020-04-10 18:35:48 +0300

    unit_test - add ExitCode enum

--- unit_test.py
+++ unit_test.py
@@ -1,3 +1,4 @@
+import enum
 import os
 import pytest
 from io import StringIO
@@ -14,11 +15,26 @@ class UnitTest:
         exit_code = ""
         with redirect_stdout(StringIO()), redirect_stderr(StringIO()):
             exit_code = pytest.main(["-q", f"{test_file_path}::{UnitTest.UNIT_TEST_NAME}"])
-        return exit_code == pytest.ExitCode.OK
+        return exit_code == ExitCode.OK
 
     @staticmethod
     def check_if_test_exists(test_file_path):
         exit_code = ""
         with redirect_stdout(StringIO()), redirect_stderr(StringIO()):
             exit_code = pytest.main(["-q", f"{test_file_path}::{UnitTest.UNIT_TEST_NAME}"])
-        return exit_code not in [pytest.ExitCode.NO_TESTS_COLLECTED, pytest.ExitCode.USAGE_ERROR]
+        return exit_code not in [ExitCode.NO_TESTS_COLLECTED, ExitCode.USAGE_ERROR]
+
+
+class ExitCode(enum.IntEnum):
+    #: tests passed
+    OK = 0
+    #: tests failed
+    TESTS_FAILED = 1
+    #: pytest was interrupted
+    INTERRUPTED = 2
+    #: an internal error got in the way
+    INTERNAL_ERROR = 3
+    #: pytest was misused
+    USAGE_ERROR = 4
+    #: pytest couldn't find tests
+    NO_TESTS_COLLECTED = 5
