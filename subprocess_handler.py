80af7dfc7c1ae773f13b2cfd2cd80f0f2a4d8cb6
Yoni Cohen <yonicohen1993@gmail.com>
2020-04-10 12:20:42 +0300

    subprocess handler - prints running command only in verbose level

--- subprocess_handler.py
+++ subprocess_handler.py
@@ -6,7 +6,7 @@ import logger
 def run_subprocess(command, cwd=os.curdir):
     if isinstance(command, list):
         command = " ".join(command)
-    logger.info(f'Running {command} command')
+    logger.verbose(f'Running {command} command')
     pipes = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, shell=True,
                              env=os.environ)
     std_out, std_err = pipes.communicate()
