bbc5482765fc55ab46892e7dfb91a22eab454988
Gilad Guralnik <giladguralnik1994@gmail.com>
2020-04-13 16:40:54 +0300

    UnitTest - change the way pytest being called && logging enhancements

--- unit_test.py
+++ unit_test.py
@@ -1,8 +1,8 @@
 import enum
 import os
-import pytest
-from io import StringIO
-from contextlib import redirect_stdout, redirect_stderr
+from subprocess import CalledProcessError
+
+from subprocess_handler import run_subprocess
 import logger
 
 
@@ -14,18 +14,32 @@ class UnitTest:
     @staticmethod
     def trigger_test_output_test(test_file_path, working_dir="", output_file_name="", message=""):
         exit_code = None
-        unit_test_stdout = StringIO()
-        unit_test_stderr = StringIO()
-        with redirect_stdout(unit_test_stdout), redirect_stderr(unit_test_stderr):
-            command = []
-            if logger.get_log_level() != logger.DEBUG:
-                command += ['-q']
-            command += ["--disable-warnings"]
-            command += [f"{test_file_path}::{UnitTest.UNIT_TEST_NAME}"]
+        stdout = ""
+        stderr = ""
+        command = ["pytest"]
+        command += [f"{test_file_path}::{UnitTest.UNIT_TEST_NAME}"]
+        if working_dir:
             command += ["--working_dir", working_dir]
+        if output_file_name:
             command += ["--output_file_name", output_file_name]
-            exit_code = pytest.main(command)
-        logger.verbose(f"{UnitTest.__name__}: {message}{unit_test_stdout.getvalue()}\n{unit_test_stderr.getvalue()}.")
+        command = " ".join(command)
+        try:
+            stdout, stderr, exit_code = run_subprocess(command)
+        except CalledProcessError as e:
+            if e.returncode is None or e.returncode not in [code for code in ExitCode]:
+                logger.info_error(f"{UnitTest.__name__}: pytest operation failed. test failed.\n{e}")
+                return ExitCode.INTERNAL_ERROR
+            stdout = e.stdout
+            stderr = e.stderr
+            exit_code = e.returncode
+        except Exception as ex:
+            logger.info_error(f"{UnitTest.__name__}: exception thrown during pytest operation. test failed.\n{ex}")
+            return ExitCode.INTERNAL_ERROR
+        if exit_code == ExitCode.OK:
+            logger.verbose(f"{UnitTest.__name__}: test '{UnitTest.UNIT_TEST_NAME}' passed.")
+        else:
+            logger.info_error(f"{UnitTest.__name__}: test '{UnitTest.UNIT_TEST_NAME}' failed.")
+        logger.debug(f"{UnitTest.__name__}: {message}{stdout}\n{stderr}.")
         return exit_code
 
     @staticmethod
