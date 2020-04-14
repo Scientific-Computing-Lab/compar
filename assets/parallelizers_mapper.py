c7a43c508f7958272cd49dd5e965da2950f46244
Yoni Cohen <yonicohen1993@gmail.com>
2020-04-14 17:07:09 +0300

    add dummy compiler to mapper

--- assets/parallelizers_mapper.py
+++ assets/parallelizers_mapper.py
@@ -1,9 +1,11 @@
 from compilers.par4all import Par4all
 from compilers.autopar import Autopar
 from compilers.cetus import Cetus
+from compilers.dummy import Dummy
 
 
 parallelizers = dict()
 parallelizers[Autopar.NAME] = Autopar
 parallelizers[Cetus.NAME] = Cetus
 parallelizers[Par4all.NAME] = Par4all
+parallelizers[Dummy.NAME] = Dummy
--- compilers/dummy.py
+++ compilers/dummy.py
@@ -1,11 +1,12 @@
 from compilers.parallelCompiler import ParallelCompiler
 
+
 class Dummy(ParallelCompiler):
     NAME = 'dummy'
 
     def __init__(self, version, compilation_flags=None, input_file_directory=None, file_list=None,
-                 include_dirs_list=None):
-        super().__init__(version, compilation_flags, input_file_directory, file_list, include_dirs_list)
+                 include_dirs_list=None, **kwargs):
+        super().__init__(version, compilation_flags, input_file_directory, file_list, include_dirs_list, **kwargs)
 
     def compile(self):
         super().compile()
