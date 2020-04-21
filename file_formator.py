512a7da839f2976a953f35fd8b0d6cb3b98b60f5
Yoni Cohen <yonicohen1993@gmail.com>
2020-04-21 14:38:37 +0300

    File Formator - fix bug - add directives indentation

--- file_formator.py
+++ file_formator.py
@@ -1,15 +1,50 @@
 import subprocess
 from exceptions import FileError
 from subprocess_handler import run_subprocess
-import logger
+import re
+
+COMMENT_PREFIX = '//____compar____'
 
 
-def format_c_code(c_files_path_list, style_arguments='\"{AccessModifierOffset: -4, IndentWidth: 4}\"'):
+def format_c_code(c_files_path_list, style_arguments='\"{AccessModifierOffset: -4, IndentWidth: 4, ColumnLimit: 0}\"'):
     try:
         format_command = ['clang-format', '-i'] + c_files_path_list + ['-style', style_arguments]
-        stdout, stderr, ret_code = run_subprocess(format_command)
-        logger.info(stdout)
+        run_subprocess(format_command)
+        directives_handler(c_files_path_list)
+        run_subprocess(format_command)
+        directives_handler(c_files_path_list, back=True)
     except subprocess.CalledProcessError as e:
         raise FileError(f'Format Code Error: clang-format return with {e.returncode} code: {e.output}')
     except FileNotFoundError as e:
         raise FileError(e)
+
+
+def directives_handler(file_paths_list, back=False):
+    for file_path in file_paths_list:
+        with open(file_path, 'r+') as fp:
+            content = fp.read()
+            if back:
+                content = uncomment_directives(content)
+            else:
+                content = comment_directives(content)
+            fp.seek(0)
+            fp.write(content)
+            fp.truncate()
+
+
+def comment_directives(text):
+    lines = text.split('\n')
+    new_lines = []
+    for line in lines:
+        new_lines.append(re.sub(r'^[ \t]*#', f'{COMMENT_PREFIX}#', line))
+    return '\n'.join(new_lines)
+
+
+def uncomment_directives(text):
+    lines = text.split('\n')
+    new_lines = []
+    for line in lines:
+        new_lines.append(re.sub(rf'{COMMENT_PREFIX}#', '#', line))
+    return '\n'.join(new_lines)
+
+
