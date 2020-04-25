b0270789cc89c0ac4ac8a507365dd799ef442a85
Yoni Cohen <yonicohen1993@gmail.com>
2020-04-25 16:29:53 +0300

    combinator - add support for omp directive params generation

--- combinator.py
+++ combinator.py
@@ -4,6 +4,7 @@ import json
 
 COMPILATION_PARAMS_FILE_PATH = "assets/compilation_params.json"
 OMP_RTL_PARAMS_FILE_PATH = "assets/omp_rtl_params.json"
+OMP_DIRECTIVES_FILE_PATH = "assets/omp_directives_params.json"
 PARAMS_SEPARATOR = '____compar____params____separator____'
 
 
@@ -15,6 +16,8 @@ def generate_combinations():
     for param in omp_rtl_array:
         omp_rtl_params.append(generate_omp_rtl_params(param))
     omp_rtl_params = mult_lists(omp_rtl_params)
+
+    omp_directives_params = generate_omp_directive_params()
 
     with open(COMPILATION_PARAMS_FILE_PATH, 'r') as fp:
         compilation_flags_array = json.load(fp)
@@ -36,34 +39,76 @@ def generate_combinations():
 
         for compile_comb in all_compilation_params:
             for omp_rtl_comb in omp_rtl_params:
-                if not omp_rtl_comb:
-                    curr_omp_rtl_comb = []
-                else:
-                    curr_omp_rtl_comb = omp_rtl_comb.split(f"{PARAMS_SEPARATOR}")
-                if not compile_comb:
-                    curr_compile_comb = []
-                else:
-                    curr_compile_comb = compile_comb.split(f"{PARAMS_SEPARATOR}")
-                new_comb = {
-                    "compiler_name": compiler,
-                    "parameters": {
-                        "omp_rtl_params": curr_omp_rtl_comb,
-                        "omp_directives_params": [],
-                        "compilation_params": curr_compile_comb
+                for omp_directives_comb in omp_directives_params:
+                    if not omp_rtl_comb:
+                        curr_omp_rtl_comb = []
+                    else:
+                        curr_omp_rtl_comb = omp_rtl_comb.split(f"{PARAMS_SEPARATOR}")
+                    if not compile_comb:
+                        curr_compile_comb = []
+                    else:
+                        curr_compile_comb = compile_comb.split(f"{PARAMS_SEPARATOR}")
+                    if not omp_directives_comb:
+                        curr_omp_directives_comb = []
+                    else:
+                        curr_omp_directives_comb = omp_directives_comb.split(f"{PARAMS_SEPARATOR}")
+                    new_comb = {
+                        "compiler_name": compiler,
+                        "parameters": {
+                            "omp_rtl_params": curr_omp_rtl_comb,
+                            "omp_directives_params": curr_omp_directives_comb,
+                            "compilation_params": curr_compile_comb
+                        }
                     }
-                }
-                combinations.append(new_comb)
+                    combinations.append(new_comb)
     return combinations
 
 
-def generate_toggle_params_list(toggle, mandatory=False):
+def generate_omp_directive_params():
+    with open(OMP_DIRECTIVES_FILE_PATH, 'r') as fp:
+        json_omp_directives = json.load(fp)
+    parallel_directive_params = json_omp_directives['parallel']
+    for_directive_params = json_omp_directives['for']
+    omp_directives_params = []
+    omp_directives_params.extend(generate_directive_list_from_json(parallel_directive_params, 'parallel'))
+    omp_directives_params.extend(generate_directive_list_from_json(for_directive_params, 'for'))
+    return mult_lists(omp_directives_params)
+
+
+def generate_directive_list_from_json(params: dict, pragma_type: str):
+    valued_params = generate_valued_directive_params(params['valued'], pragma_type)
+    toggle_params = generate_toggle_directive_params(params['toggle'], pragma_type)
+    return valued_params + toggle_params
+
+
+def generate_valued_directive_params(valued_params_list: list, pragma_type: str):
+    params_list = []
+    for param in valued_params_list:
+        current_params = []
+        pragma_name = param['pragma']
+        for value in param['values']:
+            val = value['val']
+            if value['related']:
+                for rel_val in value['related']:
+                    current_params.append(f'{pragma_type}_{pragma_name}({val}, {rel_val})')
+            else:
+                current_params.append(f'{pragma_type}_{pragma_name}({val})')
+        params_list.append(current_params)
+    return params_list
+
+
+def generate_toggle_directive_params(toggle_params_list: list, pragma_type: str):
+    return [[f'{pragma_type}_{param}', ] for param in toggle_params_list]
+
+
+def generate_toggle_params_list(toggle: list, mandatory: bool = False):
     lst = []
     for val in toggle:
         lst.append(generate_toggle_params(val, mandatory))
     return lst
 
 
-def generate_toggle_params(toggle, mandatory=False):
+def generate_toggle_params(toggle: str, mandatory: bool = False):
     if not toggle:
         return [""]
 
@@ -74,7 +119,7 @@ def generate_toggle_params(toggle, mandatory=False):
     return lst
 
 
-def generate_valued_params(valued, mandatory=False):
+def generate_valued_params(valued: dict, mandatory: bool = False):
     if not valued:
         return [""]
 
@@ -89,14 +134,14 @@ def generate_valued_params(valued, mandatory=False):
     return lst
 
 
-def generate_valued_params_list(valued_list, mandatory=False):
+def generate_valued_params_list(valued_list: list, mandatory: bool = False):
     lst = []
     for valued in valued_list:
         lst.append(generate_valued_params(valued, mandatory))
     return lst
 
 
-def generate_omp_rtl_params(param):
+def generate_omp_rtl_params(param: dict):
     if not param:
         return [""]
     lst = []
@@ -106,9 +151,9 @@ def generate_omp_rtl_params(param):
     return lst
 
 
-def mult_lists(lst):
+def mult_lists(lst: list):
     mult_list = []
     for i in itertools.product(*lst):
-        i = list(filter(None, list(i)))  # filter empty lists
+        i = list(filter(None, i))  # filter empty elements
         mult_list.append(f"{PARAMS_SEPARATOR}".join(i))
     return mult_list
