9ea96539d756452991448abdc1ce9a27e3a21aca
Gilad Guralnik <giladguralnik1994@gmail.com>
2020-04-28 15:35:48 +0300

    GUI - create external js files for form_and_errors

---
+++ GUI/static/js/multiple_files_mode/form_and_errors.js
@@ -0,0 +1,79 @@
+function handleErrors(errors){
+    console.log("handling errors");
+    console.log(errors);
+    if(errors && errors.input_directory){
+        document.getElementById('inputDirectoryAlert').innerHTML = errors.input_directory;
+    }
+    else {
+        document.getElementById('inputDirectoryAlert').innerHTML = "";
+    }
+    if(errors && errors.output_directory){
+        document.getElementById('outputDirectoryAlert').innerHTML = errors.output_directory;
+    }
+    else {
+        document.getElementById('outputDirectoryAlert').innerHTML = "";
+    }
+    if(errors && errors.main_file_path){
+        document.getElementById('mainFileAlert').innerHTML = errors.main_file_path;
+    }
+    else {
+        document.getElementById('mainFileAlert').innerHTML = "";
+    }
+    if (errors && errors.test_path){
+        document.getElementById('validationPathAlert').innerHTML = errors.test_path;
+    }
+    else{
+        document.getElementById('validationPathAlert').innerHTML = "";
+    }
+    if(errors && errors.slurm_partition){
+        document.getElementById('slurmPartitionAlert').innerHTML = errors.slurm_partition;
+    }
+    else{
+        document.getElementById('slurmPartitionAlert').innerHTML = "";
+    }
+}
+
+$(document).ready(function() {
+    $('form').submit(function (e) {
+        var url = "/multiple_files_submit"; // send the form data here.
+        if(!comparIsRunning){
+            submitForm(url);
+        }
+        e.preventDefault(); // block the traditional submission of the form.
+    });
+});
+
+function submitForm(url){
+console.log($('form').serialize());
+var formData = new FormData($('#form')[0]);
+$.ajax({
+            type: "POST",
+            url: url,
+            contentType: false,
+            processData: false,
+            data: formData, // serializes the form's elements.
+            success: function (data) {
+                console.log("received: ")
+                console.log(data)  // display the returned data in the console.
+            },
+            error: function(error){
+                console.log("received errors: ")
+                console.log(error)
+            }
+        })
+        .done(function(data) {
+            handleErrors(data.errors);
+            if(!data.errors){
+                run();
+            }
+        });
+
+    // Inject our CSRF token into our AJAX request.
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", "{{ form.csrf_token._value() }}")
+            }
+        }
+    })
+}
---
+++ GUI/static/js/single_file_mode/form_and_errors.js
@@ -0,0 +1,62 @@
+function handleErrors(errors){
+    console.log("handling errors");
+    console.log(errors);
+    if (errors && errors.test_path){
+        document.getElementById('validationPathAlert').innerHTML = errors.test_path;
+    }
+    else{
+        document.getElementById('validationPathAlert').innerHTML = "";
+    }
+    if(errors && errors.slurm_partition){
+        document.getElementById('slurmPartitionAlert').innerHTML = errors.slurm_partition;
+    }
+    else{
+        document.getElementById('slurmPartitionAlert').innerHTML = "";
+    }
+}
+
+$(document).ready(function() {
+    $('form').submit(function (e) {
+        var url = "/single_file_submit"; // send the form data here.
+        if(!comparIsRunning){
+            source_editor.innerHTML = ($('.CodeMirror')[0].CodeMirror).getValue();
+            submitForm(url);
+        }
+        e.preventDefault(); // block the traditional submission of the form.
+    });
+});
+
+function submitForm(url){
+console.log($('form').serialize());
+var formData = new FormData($('#form')[0]);
+$.ajax({
+            type: "POST",
+            url: url,
+            contentType: false,
+            processData: false,
+            data: formData, // serializes the form's elements.
+            success: function (data) {
+                console.log("received: ")
+                console.log(data)  // display the returned data in the console.
+            },
+            error: function(error){
+                console.log("received errors: ")
+                console.log(error)
+            }
+        })
+        .done(function(data) {
+            handleErrors(data.errors);
+            if(!data.errors){
+                run();
+            }
+        });
+
+    // Inject our CSRF token into our AJAX request.
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", "{{ form.csrf_token._value() }}")
+            }
+        }
+    })
+}
