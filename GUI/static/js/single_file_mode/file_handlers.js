2a63620a01b1557a649ff1a5dd835b9e34c9e190
Gilad Guralnik <giladguralnik1994@gmail.com>
2020-05-02 15:08:40 +0300

    GUI - remove unnecessary prints to console

--- GUI/static/js/single_file_mode/file_handlers.js
+++ GUI/static/js/single_file_mode/file_handlers.js
@@ -2,10 +2,8 @@ function Upload_file(fileId, textAreaId) {
         var file = document.getElementById(fileId).files[0];
         var code_mirror_source_editor = $('.CodeMirror')[0].CodeMirror;
         if(file){
-            console.log("js file test " + file.name);
             var reader = new FileReader();
             reader.readAsText(file, "UTF-8");
-             console.log("upload file function");
              reader.onload = function (evt) {
                 document.getElementById('source_editor').innerHTML = evt.target.result;
                 code_mirror_source_editor.setValue(evt.target.result);
@@ -33,6 +31,5 @@ function downloadFile(){
  }
 
  upload_source_file.onchange = function(){
-    console.log("upload file on change triggered");
     Upload_file('upload_source_file','source_file_code');
 }
